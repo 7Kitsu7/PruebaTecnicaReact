44c510af0df4bced3b1e493aa7d2b1efae2a2d01
7Kitsu7 <t1513300321@unitru.edu.pe>
2026-03-02 07:59:15 -0500

    Update commentsApi.js

--- prueba/src/features/comments/api/commentsApi.js
+++ prueba/src/features/comments/api/commentsApi.js
@@ -1,5 +1,5 @@
 import { commentApi } from '../../../lib/axios'; 
-import { pokeApi } from '../../../lib/axios'; 
+
 export const postOpinion = async (opinionData) => {
   const { data } = await commentApi.post('/posts', opinionData);
   return data;
